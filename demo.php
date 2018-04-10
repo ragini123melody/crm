e2ac8a780af22b4ba7e2e6ce963d662a049fb4d9
Ragini Shrivastava <ragini6may@gmail.com>
2018-04-10 16:35:33 +0530

    Update one

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+
+$example = "My Example";
+echo 'single-quotes-$example'."<br>"; 
+echo "Double-quotes-$example"."<br";
+
+
+?>
--- kunal/demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$example = "My Example";
-echo 'single-quotes-$example'."<br>"; 
-echo "Double-quotes-$example"."<br";
-
-
-?>
